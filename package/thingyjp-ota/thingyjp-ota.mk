5fce4dbdaf40eb288451279ddddf15b0e3abca10
Daniel Palmer <daniel@0x0f.com>
2018-08-01 20:08:58 +0900

    Wire stamp into build process

--- package/thingyjp-ota/thingyjp-ota.mk
+++ package/thingyjp-ota/thingyjp-ota.mk
@@ -17,10 +17,19 @@ HOST_THINGYJP_OTA_CONF_OPTS += -Dhost=true
 
 define THINGYJP_OTA_INSTALLKEYS
 	mkdir -p $(TARGET_DIR)/etc/thingyjp/ota/keys
-	cp $(BR_PACKAGE_THINGYJP_OTA_KEYSDIR)/rsa.pub $(TARGET_DIR)/etc/thingyjp/ota/keys/
+	cp $(BR2_PACKAGE_THINGYJP_OTA_KEYSDIR)/rsa.pub $(TARGET_DIR)/etc/thingyjp/ota/keys/
+endef
+
+define THINGYJP_OTA_STAMPIMAGE
+        mkdir -p $(TARGET_DIR)/etc/thingyjp/ota/
+	$(HOST_DIR)/bin/ota_stamp -t $(TARGET_DIR) -r $(BR2_PACKAGE_THINGYJP_OTA_REPODIR)
 endef
 
 THINGYJP_OTA_POST_INSTALL_TARGET_HOOKS += THINGYJP_OTA_INSTALLKEYS
+
+ifeq ($(BR2_PACKAGE_THINGYJP_OTA_STAMP),y)
+	THINGYJP_OTA_POST_INSTALL_TARGET_HOOKS += THINGYJP_OTA_STAMPIMAGE
+endif
 
 $(eval $(meson-package))
 $(eval $(host-meson-package))
